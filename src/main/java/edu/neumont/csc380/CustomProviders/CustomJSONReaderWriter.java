bb8fe65ed041c8ba6a98c0ce88f9b09e9a2be7ea
Adam <adamhomer88@gmail.com>
2014-04-28 10:21:14 -0600

    Rollback and added fix for conflict between updateing a user'

---
+++ src/main/java/edu/neumont/csc380/CustomProviders/CustomJSONReaderWriter.java
@@ -0,0 +1,10 @@
+package edu.neumont.csc380.CustomProviders;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+public class CustomJSONReaderWriter extends JacksonJsonProvider {
+
+	public CustomJSONReaderWriter(){
+		
+	}
+}
