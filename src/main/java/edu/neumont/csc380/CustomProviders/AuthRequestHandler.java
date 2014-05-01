d23c6bf67e75560825e9a437d08ca7f6d0395841
Adam <adamhomer88@gmail.com>
2014-05-01 09:53:17 -0600

    Finished authorization for both other servers and external clients.

--- src/main/java/edu/neumont/csc380/CustomProviders/AuthRequestHandler.java
+++ src/main/java/edu/neumont/csc380/CustomProviders/AuthRequestHandler.java
@@ -1,5 +1,8 @@
 package edu.neumont.csc380.CustomProviders;
 
+import java.util.ArrayList;
+import java.util.TreeMap;
+
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
@@ -11,13 +14,31 @@ import org.apache.cxf.message.Message;
 public class AuthRequestHandler implements RequestHandler{
 
 	public Response handleRequest(Message arg0, ClassResourceInfo arg1) {
-		String method = (String) arg0.get(arg0.HTTP_REQUEST_METHOD);
+		String method = (String) arg0.get(Message.HTTP_REQUEST_METHOD);
 		if(method.equals("PUT") || method.equals("DELETE"))
 		{
-			System.out.println("put method ran");
+			String token = getAuthToken(arg0);
+			boolean authenticated = authenticateRequest(token);
+			if(!authenticated)
+				return Response.status(Response.Status.UNAUTHORIZED).build();
 		}
-		System.out.println("Authorized");
+		System.out.println("Authorized User");
 		return null;
+	}
+
+	private String getAuthToken(Message arg0) {
+		TreeMap stuff = (TreeMap)arg0.get(Message.PROTOCOL_HEADERS);
+		ArrayList<String> morestuff = (ArrayList<String>) stuff.get("Authorization");
+		return morestuff.get(0);
+	}
+
+	private boolean authenticateRequest(String token) {
+		boolean isAuthenticated = false;
+		if(token != null){ isAuthenticated = true; }
+		//compare tokens
+		else{}
+		//authenticateUserUsingAuthClient
+		return isAuthenticated;
 	}
 
 }
